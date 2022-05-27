db6b24de039f3fbe7f9be1f7e984e446793e1b6f
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2022-05-27 20:33:24 -0300

    feat: adding styled-components

--- App.tsx
+++ App.tsx
@@ -1,21 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World 🚀🚀🚀</Text>
-      <StatusBar style='dark' translucent backgroundColor='#ccc' />
-    </View>
+    <>
+      <StatusBar style='dark' translucent />
+      <Dashboard />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
